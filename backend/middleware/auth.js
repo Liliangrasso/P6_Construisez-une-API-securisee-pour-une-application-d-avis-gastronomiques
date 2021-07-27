7561c401ee63ccd3eb0f29dd0fba55bda6353756
Liliangrasso <lilian.grasso.pro@gmail.com>
2021-07-27 10:35:44 +0200

    :hammer: Ajouts des middlewares

---
+++ backend/middleware/auth.js
@@ -0,0 +1,24 @@
+﻿/*** IMPORTATIONS ****/
+const jwt = require('jsonwebtoken');
+
+/*** Auth ****/
+module.exports = (req, res, next) => {
+    try {
+        /*** Extraction du token dans le header ***/
+        const token = req.headers.authorization.split(' ')[1];
+        /*** On decode le token ***/
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        /*** Extraction de l'id utilisateur depuis le token ***/
+        const userId = decodedToken.userId;
+        /*** Comparaison des id ***/
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!')
+        });
+    }
+};
