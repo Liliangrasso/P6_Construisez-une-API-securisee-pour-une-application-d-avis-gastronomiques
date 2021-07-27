90d2e291607a621e5f72371a48e1c429b318565f
Liliangrasso <lilian.grasso.pro@gmail.com>
2021-07-27 10:34:55 +0200

    :hammer: Ajouts des models

---
+++ backend/model/Sauce.js
@@ -0,0 +1,18 @@
+﻿/*** IMPORTATIONS ****/
+const mongoose = require('mongoose');
+
+/*** SCHEMA ****/
+const sauceSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    heat: { type: Number, required: false },
+    likes: { type: Number, default: 0},
+    dislikes: { type: Number},
+    imageUrl: { type: String, required: false },
+    mainPepper: { type: String, required: false },
+    usersLiked: { type: Array, default: [], required: true },
+    usersDisliked: { type: Array, default: [], required: true },
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
---
+++ backend/model/User.js
@@ -0,0 +1,14 @@
+﻿/*** IMPORTATIONS ****/
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+/*** SCHEMA ****/
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+// Vérifie que l'email est unique
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
