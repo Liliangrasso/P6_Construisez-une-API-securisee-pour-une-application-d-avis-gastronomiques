16efaba706e56a8007785408cd29c0e7992132ec
Liliangrasso <lilian.grasso.pro@gmail.com>
2021-07-27 10:36:30 +0200

    :hammer: Ajouts des routes

---
+++ backend/routes/sauce.js
@@ -0,0 +1,15 @@
+﻿/*** IMPORTATIONS ****/
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const SauceCtrl = require('../controllers/sauce');
+
+/*** Routes ****/
+router.get('/', auth, SauceCtrl.getAllSauce);
+router.post('/', auth, multer, SauceCtrl.createSauce);
+router.get('/:id', auth, SauceCtrl.getOneSauce);
+router.put('/:id', auth, SauceCtrl.modifySauce);
+router.delete('/:id', auth, SauceCtrl.deleteSauce);
+router.post('/:id/like', auth, SauceCtrl.like);
+module.exports = router;
